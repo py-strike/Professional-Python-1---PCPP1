6255b3cd1c695c3c065a64cc490e56a162bf3be2
Khaled <codepower.help@gmail.com>
2025-02-16 09:23:46 +0100

    add: instance method

--- 1. OOP Basics/oop_basics.py
+++ 1. OOP Basics/oop_basics.py
@@ -3,5 +3,9 @@ class Person:
         self.name = name
         self.age = age
 
+    def greet(self):
+        print(f"Hello, my name is {self.name}")
+
 person_1 = Person("khaled", 21)
 print(person_1.age)
+person_1.greet()
